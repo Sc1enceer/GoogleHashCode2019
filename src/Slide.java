6a806fb755617973d211a84ee2e6562925f06674
Alvin <alvin10558310@outlook.com>
2019-02-28 19:42:10 +0000

    Created Parser, Photo, Slide + handled parsing files

---
+++ src/Slide.java
@@ -0,0 +1,41 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Slide {
+    Photo[] photos;
+    Set<String> tags = new HashSet<String>();
+
+    public Slide(Photo[] photos) {
+        this.photos = photos;
+        generateTags();
+    }
+
+    /*
+        this function
+     */
+    private void generateTags() {
+        if(photos[1] == null){
+            this.tags = photos[0].getTags();
+        } else {
+            this.tags.addAll(photos[0].getTags());
+            this.tags.addAll(photos[1].getTags());
+        }
+    }
+
+
+    public Photo[] getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(Photo[] photos) {
+        this.photos = photos;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+}
